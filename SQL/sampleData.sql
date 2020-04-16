05e9bf96e7715582e104fe58bef5be26b5437d21
Yu-Kai Yang <clarkyang@cmail.carleton.ca>
2020-04-16 17:06:04 -0400

    bug fix and more sample data

--- SQL/sampleData.sql
+++ SQL/sampleData.sql
@@ -9,6 +9,7 @@ insert into Publisher values ('Dog Publisher', '123 Zoo Rd', 'editor@somewhere.c
 
 insert into Book values ('How to train your dog', 'John Smith', 123456789000, 312, 12.95, 'Animal', 'Dog Publisher', 10.2, 10);
 insert into Book values ('How to train your cat', 'Jane Smith', 123456789222, 123, 13.95, 'Animal', 'Dog Publisher', 15.0, 15);
+insert into Book values ('How computer works', 'Meow Woof', 123456789213, 777, 5.95, 'Technology', 'Dog Publisher', 15.0, 15);
 
 insert into User_table values ('default', 'password', 'invalid@user.org');
 insert into User_table values ('default2', 'password', 'invalid2@user.org');
@@ -17,3 +18,4 @@ insert into Orders values (22, 'CA777766543', 'default', '321 Owls cabin Rd', 12
 
 insert into Order_Item values (22, 123456789000);
 insert into Order_Item values (22, 123456789222);
+insert into Order_Item values (22, 123456789213);
--- src/PostgreSqlQuery.java
+++ src/PostgreSqlQuery.java
@@ -14,6 +14,7 @@ public class PostgreSqlQuery {
 	private static void checkBookStock(String isbn, int stock) {
 		if (stock < 10) {
 			// email the publisher...
+			// emailPublisherForStock(isbn);
 		}
 	}
 
@@ -28,7 +29,7 @@ public class PostgreSqlQuery {
 					.executeQuery("SELECT * FROM Book where " + category + " = '" + criteria + "'");
 			dumpAllResult(resultSet, "book");
 		} catch (SQLException e) {
-			//TODO: add proper error message...
+			// TODO: add proper error message...
 			e.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -55,12 +56,13 @@ public class PostgreSqlQuery {
 		ResultSetMetaData rsmd = resultSet.getMetaData();
 		int columnsNumber = rsmd.getColumnCount();
 		while (resultSet.next()) {
-		    for (int i = 1; i <= columnsNumber; i++) {
-		        if (i > 1) System.out.print(",  ");
-		        String columnValue = resultSet.getString(i);
-		        System.out.print(columnValue + " " + rsmd.getColumnName(i));
-		    }
-		    System.out.println("");
+			for (int i = 1; i <= columnsNumber; i++) {
+				if (i > 1)
+					System.out.print(",  ");
+				String columnValue = resultSet.getString(i);
+				System.out.print(columnValue + " " + rsmd.getColumnName(i));
+			}
+			System.out.println("");
 		}
 	}
 
@@ -71,16 +73,17 @@ public class PostgreSqlQuery {
 		try {
 			// create an order, get order number
 			ResultSet resultSet = statement.executeQuery(
-					"Insert into Orders (Tracking_number, Username) values (SOME_TRACKING#, 'default') RETURNING order_number");
+					"Insert into Orders (Tracking_number, Username, Billing_Address, Credit_card, Shipping_Address) values ('SOMETRACKING', 'default', 'Somewhere', 123, 'Elsewhere') RETURNING order_number");
 			resultSet.next();
 			int order_number = resultSet.getInt(1);
 			// decrement the book stock by 1
-			statement.executeQuery("UPDATE Book\r\n" + "SET stock = stock - 1\r\n" + "WHERE Isbn =" + isbn);
-			// add the book into the order with the isbn and order number
-			resultSet = statement.executeQuery("Insert into Order_Item (Order_number, ISBN) values (" + order_number
-					+ ", " + isbn + ") RETURNING Stock");
+			resultSet = statement
+					.executeQuery("UPDATE Book SET stock = stock - 1 WHERE Isbn =" + isbn + "RETURNING STOCK");
 			resultSet.next();
 			int stock = resultSet.getInt(1);
+			// add the book into the order with the isbn and order number
+			statement.executeUpdate(
+					"Insert into Order_Item (Order_number, ISBN) values (" + order_number + ", " + isbn + ")");
 			checkBookStock(isbn, stock);
 			System.out.println("Order has been created for book " + isbn + ", order number is " + order_number);
 		} catch (Exception e) {
@@ -119,7 +122,6 @@ public class PostgreSqlQuery {
 
 	private static void showSales() {
 		try {
-			// create an order, get order number
 			ResultSet resultSet = statement.executeQuery(
 					"SELECT COUNT(*), genre\r\n" + "FROM Order_Item natural\r\n" + "JOIN book group by genre");
 			dumpAllResult(resultSet, "N/A");
@@ -137,7 +139,7 @@ public class PostgreSqlQuery {
 			statement.executeUpdate("INSERT INTO Book Values (" + input + ")");
 			System.out.println("Book has been added");
 		} catch (SQLException e) {
-			//TODO: add proper error message...
+			// TODO: add proper error message...
 			e.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -145,33 +147,32 @@ public class PostgreSqlQuery {
 	}
 
 	private static void deleteBOok() {
-		System.out.println(
-				"ISBN of the book to be removed");
+		System.out.println("ISBN of the book to be removed");
 		String isbn = sc.nextLine(); // Read user input
 
 		try {
 			statement.executeUpdate("DELETE FROM Book WHERE isbn = " + isbn);
 			System.out.println("Book has been removed");
 		} catch (SQLException e) {
-			//TODO: add proper error message...
+			// TODO: add proper error message...
 			e.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private static void showAllBooks() {
 		try {
 			ResultSet resultSet = statement.executeQuery("SELECT * FROM BOOK");
 			dumpAllResult(resultSet, "N/A");
 		} catch (SQLException e) {
-			//TODO: add proper error message...
+			// TODO: add proper error message...
 			e.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private static void ownerMenu() {
 		System.out.println("1. Show sales per genre");
 		System.out.println("2. Add a new book");
@@ -229,7 +230,6 @@ public class PostgreSqlQuery {
 			} catch (Exception e) {
 				System.out.println("Invalid option");
 			}
-
 		}
 	}
 }
